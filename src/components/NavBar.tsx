8a18c09f279d076482ca1643e43244ece024a35c
Nico Neiman <niconeiman@gmail.com>
2021-09-25 17:28:08 -0500

    navBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavBarItem } from "../models/navBar.model";
+import { v4 as uuidv4 } from "uuid";
+
+type NabBarProps = {
+	menuItems: NavBarItem[];
+};
+
+const NavBar: React.FC<NabBarProps> = ({ menuItems }) => {
+	return (
+		<div className='navBar'>
+			<div className='navBar-logo-and-items'>
+				Logo
+				<ul>
+					{menuItems.map((item: NavBarItem) => (
+						<li key={uuidv4()}>{item.label}</li>
+					))}
+				</ul>
+			</div>
+			<div className='navBar-avatar'>Avatar</div>
+		</div>
+	);
+};
+
+export default NavBar;
--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -2,22 +2,18 @@ import React from "react";
 import { TitleMedium } from ".";
 
 type ThumbnailProps = {
-    imageURL: string;
-    alt: string;
-    title: string;
-}
+	imageURL: string;
+	alt: string;
+	title: string;
+};
 
-const Thumbnail: React.FC<ThumbnailProps> = ({imageURL, alt, title}) => {
-    return (
-        <div className={'thumbnail'}>
-            <img src={imageURL}
-                alt={alt}
-                className={'thumbnail-image'}/>
-            <TitleMedium class={'thumbnail-title'}>{title}</TitleMedium>
-        </div>
-    );
-}
+const Thumbnail: React.FC<ThumbnailProps> = ({ imageURL, alt, title }) => {
+	return (
+		<div className={"thumbnail"}>
+			<img src={imageURL} alt={alt} className={"thumbnail-image"} />
+			<TitleMedium class={"thumbnail-title"}>{title}</TitleMedium>
+		</div>
+	);
+};
 
-export {
-    Thumbnail
-}
+export { Thumbnail };
