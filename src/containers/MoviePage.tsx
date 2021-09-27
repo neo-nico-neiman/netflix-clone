5de61f0e98d5282587b50286e74fe6ff039d3053
Nico Neiman <niconeiman@gmail.com>
2021-09-27 15:55:08 -0500

    layout

--- src/containers/MoviePage.tsx
+++ src/containers/MoviePage.tsx
@@ -28,69 +28,59 @@ const MoviePage = () => {
 			style={{ backgroundImage: `url(../${selectedMovie.imageURL}` }}
 		>
 			<div className='movie-page-text'>
-				<TitleXLarge class={"movie-page-text-title"}>
-					{selectedMovie.title.toUpperCase()}
-				</TitleXLarge>
-				<div className='movie-page-text-movie-specs'>
-					<TextContent class={"movie-page-text-movie-specs-status"}>
-						New
-					</TextContent>
-					<TextContent>2021</TextContent>
-					<TextContent class={"movie-page-text-movie-specs-rated"}>
-						TV-MA
-					</TextContent>
-					<TextContent>Limited Series</TextContent>
-					<TextContent class={"movie-page-text-movie-specs-rated"}>
-						4K HDR
-					</TextContent>
-				</div>
-				{/* <div className='hero-text-buttons'>
-					<ButtonSecondary
-						label={"Play"}
-						onClick={onPlay}
-						className=''
-					></ButtonSecondary>
+				<div className='movie-page-text-container'>
+					<TitleXLarge class={"movie-page-text-title"}>
+						{selectedMovie.title.toUpperCase()}
+					</TitleXLarge>
+					<div className='movie-page-text-movie-specs'>
+						<TextContent class={"movie-page-text-movie-specs-status"}>
+							New
+						</TextContent>
+						<TextContent>2021</TextContent>
+						<TextContent class={"movie-page-text-movie-specs-rated"}>
+							TV-MA
+						</TextContent>
+						<TextContent>Limited Series</TextContent>
+						<TextContent class={"movie-page-text-movie-specs-rated"}>
+							4K HDR
+						</TextContent>
+					</div>
 
-					<ButtonSecondary
-						label={"My List"}
-						onClick={onMyList}
-						className='margin-left-sm'
-					></ButtonSecondary>
-				</div> */}
-				<TextContent class={"movie-page-text-description"}>
-					{selectedMovie.description}
-				</TextContent>
+					<TextContent class={"movie-page-text-description"}>
+						{selectedMovie.description}
+					</TextContent>
 
-				<div className='movie-page-crew'>
-					<TextContent>Cast: {nameGenerator(3)}</TextContent>
-					<TextContent>Director: {nameGenerator()} </TextContent>
-					<TextContent>Creator: {nameGenerator()}</TextContent>
-				</div>
-				<div className='movie-page-CTA'>
-					<ButtonHollowBorder
-						label='Rate This Title'
-						icon='thumbs'
-						onClick={onClick()}
-						className=''
-					></ButtonHollowBorder>
-					<ButtonHollowBorder
-						label='Play'
-						icon='play'
-						onClick={onClick()}
-						className=''
-					></ButtonHollowBorder>
-					<ButtonHollowBorder
-						label='More Like This'
-						icon='grid'
-						onClick={onClick()}
-						className=''
-					></ButtonHollowBorder>
-					<ButtonHollowBorder
-						label={"Audio & Subtitles"}
-						icon='subtitles'
-						onClick={onClick()}
-						className=''
-					></ButtonHollowBorder>
+					<div className='movie-page-crew'>
+						<TextContent>Cast: {nameGenerator(3)}</TextContent>
+						<TextContent>Director: {nameGenerator()} </TextContent>
+						<TextContent>Creator: {nameGenerator()}</TextContent>
+					</div>
+					<div className='movie-page-CTA'>
+						<ButtonHollowBorder
+							label='Rate This Title'
+							icon='thumbs'
+							onClick={onClick()}
+							className=''
+						></ButtonHollowBorder>
+						<ButtonHollowBorder
+							label='Play'
+							icon='play'
+							onClick={onClick()}
+							className=''
+						></ButtonHollowBorder>
+						<ButtonHollowBorder
+							label='More Like This'
+							icon='grid'
+							onClick={onClick()}
+							className=''
+						></ButtonHollowBorder>
+						<ButtonHollowBorder
+							label={"Audio & Subtitles"}
+							icon='subtitles'
+							onClick={onClick()}
+							className=''
+						></ButtonHollowBorder>
+					</div>
 				</div>
 			</div>
 		</div>
